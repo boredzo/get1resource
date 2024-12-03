d98e72a4875f4fd97ad2a3a7f2f46482aa27c827
Peter Hosey <git@boredzo.org>
2024-12-03 05:44:01 -0800

    Initial Commit

---
+++ get1Resource/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  get1Resource
+//
+//  Created by Peter Hosey on 2024-12-03.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+	@autoreleasepool {
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	}
+	return 0;
+}
